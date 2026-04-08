d55f97c41ea136bc8cfcdacdfe7348f6de69d308
philly88r <150537240+philly88r@users.noreply.github.com>
2026-04-08 14:44:41 -0400

    Fix site config for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://carbidesawsinc.com',
+  integrations: [tailwind()],
 });
